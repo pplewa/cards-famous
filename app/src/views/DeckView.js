93f50d144c083ca352019a9ceaedf3bb1cab3e6e
Piotr Plewa <pplewa@me.com>
2014-05-26 15:53:12 +1000

    counter, looping after finished deck

--- app/src/views/DeckView.js
+++ app/src/views/DeckView.js
@@ -39,19 +39,18 @@ define(function(require, exports, module) {
             // align: [0.5, 0.5]
         });
 
-        this.mainNode = this.add(this.rootModifier);
-
-        this.currentIndex = 0;
-        _createDeck.call(this);
-
         var deckContainer = new ContainerSurface({
+            size: [190, 290],
             properties: {
                 overflow: 'hidden'
             }
         });
 
-        // this.mainNode.add(new CardView());
-        // deckContainer.add(new CardView());
+        this.mainNode = this.add(this.rootModifier);
+
+        this.currentIndex = 0;
+        _createDeck.call(this);
+        _createCounter.call(this);
     }
 
     DeckView.prototype = Object.create(View.prototype);
@@ -60,61 +59,38 @@ define(function(require, exports, module) {
     DeckView.DEFAULT_OPTIONS = {
     };
 
-    DeckView.prototype.showCurrentCard = function() {
-        this.ready = false;
-
-        var card = this.cards[this.currentIndex];
-        this.lightbox.show(card, function() {
-            this.ready = true;
-            // slide.fadeIn();
-        }.bind(this));
-    };
-
     DeckView.prototype.showNextCard = function() {
         this.currentIndex += 1;
-        console.log('next');
-        this.showCard();
-        // this.lightbox._showing = false;
-
-        // if (!this.ready) return;
-
-        // if (this.currentIndex === this.cards.length) this.currentIndex = 0;
-        // this.showCurrentCard();
+        if (this.currentIndex > 1) {
+            var index = (this.currentIndex + 1) % 3;
+            var card = this.cards[this.currentIndex + 1];
+            if (card) {
+                card.drag.setPosition([0, 0]);
+                this.renderNodes[index].set(card);
+            } 
+            if (!this.cards[this.currentIndex]) {
+                this.currentIndex = 0;
+                for (var i = 0; i < 3; i++) {
+                    this.cards[i].drag.setPosition([0, 0]);
+                    this.renderNodes[i].set(this.cards[i]);
+                }
+            }
+        }
+        this.counter.setContent(this.currentIndex + 1);
     };
 
     DeckView.prototype.showPrevCard = function() {
         this.currentIndex -= 1;
-        console.log('prev');
-        // this.lightbox._showing = false;
-
-        // if (!this.ready) return;
-
-        // if (this.currentIndex === this.cards.length) this.currentIndex = 0;
-        // this.showCurrentCard();
-    };
-
-    DeckView.prototype.showCard = function() {
-        if (this.currentIndex > 1) {
-            var index = (this.currentIndex + 1) % 3;
-            var card = this.cards[this.currentIndex + 1];
-            card.drag.setPosition([0, 0]);
-            this.renderNodes[index].set(card);
-        }
-
-        this.cards[this.currentIndex].drag.sync.on('update', function(evt){
-            if (this.cards[this.currentIndex].startDirection < 0) return;
-            this.cards[this.currentIndex - 1].drag.setPosition([evt.position[0] - 225, 0]);
-        }.bind(this));
-
-        this.cards[this.currentIndex].drag.sync.on('end', function(data){
-            if (this.cards[this.currentIndex].startDirection < 0) return;
-            if (data.position[0] < 100) {
-                this.cards[this.currentIndex - 1].drag.setPosition([-225, 0], { duration: 100 });
-            } else {
-                this.cards[this.currentIndex - 1].drag.setPosition([0, 0], { duration: 100 });
-                this.showPrevCard();
+        // this._eventOutput.emit('updateCounter')
+        if (this.currentIndex > 0) {
+            var index = (this.currentIndex - 1) % 3;
+            var card = this.cards[this.currentIndex - 1];
+            if (card) {
+                card.drag.setPosition([-225, 0]);
+                this.renderNodes[index].set(card);
             }
-        }.bind(this));
+        }
+        this.counter.setContent(this.currentIndex + 1);
     };
 
     function _createDeck() {
@@ -122,6 +98,24 @@ define(function(require, exports, module) {
         for (var i = 0; i < CARDS.length; i++) {       
             var cardView = new CardView({ title: CARDS[i].title, transform: Transform.translate(0, 0, -i*0.1) });
             cardView.on('nextCard', this.showNextCard.bind(this));
+
+            cardView.drag.sync.on('update', function(evt){
+                var previousCard = this.cards[this.currentIndex - 1];
+                if (cardView.startDirection < 0 || !previousCard) return;
+                previousCard.drag.setPosition([evt.position[0] - 225, 0]);
+            }.bind(this));
+
+            cardView.drag.sync.on('end', function(data){
+                var previousCard = this.cards[this.currentIndex - 1];
+                if (cardView.startDirection < 0 || !previousCard) return;
+                if (data.position[0] < 100) {
+                    previousCard.drag.setPosition([-225, 0], { duration: 100 });
+                } else {
+                    previousCard.drag.setPosition([0, 0], { duration: 100 });
+                    this.showPrevCard();
+                }
+            }.bind(this));
+
             this.cards.push(cardView);
         }
 
@@ -132,22 +126,23 @@ define(function(require, exports, module) {
             renderNode.add(this.cards[i])
             this.mainNode.add(renderNode);
         }
-        
-        // this.cards[2].drag.setPosition([-225, 0]);
-        // this.cards[1].drag.sync.on('update', function(evt){
-        //     if (this.cards[1].startDirection < 0) return;
-        //     this.cards[0].drag.setPosition([evt.position[0] - 225, 0]);
-        // }.bind(this));
+    }
 
-        // this.cards[1].drag.sync.on('end', function(data){
-        //     if (this.cards[1].startDirection < 0) return;
-        //     if (data.position[0] < 100) {
-        //         this.cards[0].drag.setPosition([-225, 0], { duration: 100 });
-        //     } else {
-        //         this.cards[0].drag.setPosition([0, 0], { duration: 100 });
-        //         this.showPrevCard();
-        //     }
-        // }.bind(this));
+    function _createCounter() {
+        this.counterModifier = new StateModifier({
+            origin: [0.5, 0.75]
+        });
+
+        this.counter = new Surface({
+            size: [100, 20],
+            content: this.currentIndex + 1,
+            properties: {
+                background: '#ccc',
+                textAlign: 'center'
+            }
+        });
+
+        this.mainNode.add(this.counterModifier).add(this.counter);
     }
 
     module.exports = DeckView;
